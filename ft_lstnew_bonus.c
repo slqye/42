6af3805bb446c3323a789cb7e60365d8ae3cd3af
Slqye <69047488+slqye@users.noreply.github.com>
2026-01-07 10:12:58 +0100

    feat: project files

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 13:42:50 by uwywijas          #+#    #+#             */
+/*   Updated: 2023/12/08 17:08:27 by uwywijas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*result;
+
+	result = malloc(sizeof(t_list));
+	if (result == NULL)
+		return (NULL);
+	result->content = content;
+	result->next = NULL;
+	return (result);
+}

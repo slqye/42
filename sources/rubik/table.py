4a050acd3f8ca3108a104e61676676bec5cf46d1
Slqye <69047488+slqye@users.noreply.github.com>
2026-01-22 12:12:18 +0100

    feat: cleaning code

--- sources/rubik/table.py
+++ sources/rubik/table.py
@@ -48,60 +48,58 @@ class Table:
 		Compute phase 2 (g1->g2) cube state.
 		"""
 		corners_encoding: int = 0
-		edges_encoding: int = 0
-		edges: list = Table.MSE_SLICES["M"]
+		edge_encodings: int = 0
 
-		for index, i in enumerate(cube._corner_orientations):
-			corners_encoding *= 3
-			corners_encoding += i
-		for index, i in enumerate(cube._edge_positions):
-			if i in edges:
-				edges_encoding |= (1 << index)
-		return corners_encoding * 2 ** 12 + edges_encoding
+		for index, corner in enumerate(cube._corner_orientations):
+			corners_encoding = corners_encoding * 3 + corner
+		for index, edge in enumerate(cube._edge_positions):
+			if edge in Table.MSE_SLICES["M"]:
+				edge_encodings |= (1 << index)
+		return corners_encoding * 2 ** 12 + edge_encodings
 
 	@staticmethod
 	def phase_3(cube: object) -> int:
 		"""
 		Compute phase 3 (g2->g3) cube state.
 		"""
-		length: int = len(cube._corner_positions)
 		edge_configuration: list = [x for x in cube._edge_positions if x not in Table.MSE_SLICES["M"]]
-		corner_encoding: int = 0
-		edges_encoding: int = 0
+		corner_lengths: int = len(cube._corner_positions)
+		corner_encodings: int = 0
+		edge_encodings: int = 0
+		combinations: list = list(itertools.combinations(range(8), 4))
 
-		for i in range(length):
-			corner_encoding *= (length - i)
-			for j in range(i + 1, length):
-				if cube._corner_positions[i] > cube._corner_positions[j]:
-					corner_encoding += 1
+		for index in range(corner_lengths):
+			corner_encodings *= (corner_lengths - index)
+			for j in range(index + 1, corner_lengths):
+				if cube._corner_positions[index] > cube._corner_positions[j]:
+					corner_encodings += 1
 		for index, edge in enumerate(edge_configuration):
 			if edge in Table.MSE_SLICES["S"]:
-				edges_encoding |= (1 << index)
-		edges_encoding = Table._rank_4_8(edges_encoding)
-		return corner_encoding * 70 + edges_encoding
+				edge_encodings |= (1 << index)
+		edge_encodings = combinations.index(tuple(i for i in range(8) if edge_encodings & (1 << i)))
+		return corner_encodings * 70 + edge_encodings
 
 	@staticmethod
 	def phase_4(cube: object) -> int:
 		"""
 		Compute phase 4 (g3->g4) cube state.
 		"""
-		corner_rank = 0
-		corners = list(cube._corner_positions)
-		for i in range(7):
-			count = 0
-			for j in range(i + 1, 8):
-				if corners[i] > corners[j]:
-					count += 1
-			corner_rank = corner_rank * (8 - i) + count
-		edge_rank = 0
-		edges = list(cube._edge_positions)
-		for i in range(11):
-			count = 0
-			for j in range(i + 1, 12):
-				if edges[i] > edges[j]:
-					count += 1
-			edge_rank = edge_rank * (12 - i) + count
-		return corner_rank * 39916800 + edge_rank
+		corner_lengths: int = len(cube._corner_positions)
+		edge_lengths: int = len(cube._edge_positions)
+		corner_encodings: int = 0
+		edge_encodings: int = 0
+
+		for index in range(corner_lengths - 1):
+			corner_encodings *= (corner_lengths - index)
+			for j in range(index + 1, corner_lengths):
+				if cube._corner_positions[index] > cube._corner_positions[j]:
+					corner_encodings += 1
+		for index in range(edge_lengths - 1):
+			edge_encodings *= (edge_lengths - index)
+			for j in range(index + 1, edge_lengths):
+				if cube._edge_positions[index] > cube._edge_positions[j]:
+					edge_encodings += 1
+		return corner_encodings * 39916800 + edge_encodings
 
 	@staticmethod
 	def _bfs(cube: object, phase: callable, moves: list) -> dict:
@@ -130,16 +128,3 @@ class Table:
 						queue.append((clone, current_cube[1] + [cube.SPINS_REVERSE[cube.SPINS.index(move)]]))
 		print()
 		return result
-
-	@staticmethod
-	def _rank_4_8(mask: int) -> int:
-		rank: int = 0
-		k: int = 4
-
-		for i in range(8):
-			if mask & (1 << i):
-				k -= 1
-			else:
-				if k > 0:
-					rank += math.comb(7 - i, k - 1)
-		return rank

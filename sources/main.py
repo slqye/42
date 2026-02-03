6e51b67ed26200228a38621b2ce97cca7e30c4ca
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-03 11:51:24 +0100

    feat: better temporary shell game

--- sources/main.py
+++ sources/main.py
@@ -1,9 +1,13 @@
+import logging
+
 from r_learn.environment import SnakeEnvironment
 from r_learn.interpreter import SnakeInterpreter
 from r_learn.agent import SnakeAgent
 
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s]: %(message)s", level=logging.DEBUG)
+
 def main():
-	environment: SnakeEnvironment = SnakeEnvironment(3)
+	environment: SnakeEnvironment = SnakeEnvironment(10)
 	interpreter: SnakeInterpreter = SnakeInterpreter(environment)
 	agent: SnakeAgent = SnakeAgent(interpreter)
 	moves: dict = {

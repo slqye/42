81d09c1cb89cb07b9cf9098dc476b71c839c94b6
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-03 16:32:22 +0100

    feat: epochs loop to try agent

--- sources/main.py
+++ sources/main.py
@@ -4,24 +4,28 @@ from r_learn.environment import SnakeEnvironment
 from r_learn.interpreter import SnakeInterpreter
 from r_learn.agent import SnakeAgent
 
-logging.basicConfig(format="[%(asctime)s] [%(levelname)s]: %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s]: %(message)s", level=logging.INFO)
+
+EPOCHS = 10000
 
 def main():
-	environment: SnakeEnvironment = SnakeEnvironment(10)
-	interpreter: SnakeInterpreter = SnakeInterpreter(environment)
-	agent: SnakeAgent = SnakeAgent(interpreter)
-	moves: dict = {
-		1: environment.ACTION_LEFT,
-		2: environment.ACTION_RIGHT,
-		3: environment.ACTION_UP,
-		4: environment.ACTION_DOWN,
-	}
+	q_table_holder: dict = None
 
-	while environment.running:
-		print(environment)
-		action: int = int(input("move: "))
-		if action == 0: break
-		environment.action(moves[action])
+	for epoch in range(EPOCHS):
+		environment: SnakeEnvironment = SnakeEnvironment(10)
+		interpreter: SnakeInterpreter = SnakeInterpreter(environment)
+		agent: SnakeAgent = SnakeAgent(interpreter)
+		if q_table_holder is not None:
+			agent._q_table = q_table_holder
+		moves: int = 0
+		while environment.running:
+			agent.perform_action(environment)
+			agent.mesure_reward(environment)
+			agent.update_q_table(environment)
+			moves += 1
+		logging.info(f"[{epoch + 1}/{EPOCHS}]: {environment.get_snake_length()}, {moves}")
+		q_table_holder = agent._q_table
+
 
 if __name__ == "__main__":
 	main()

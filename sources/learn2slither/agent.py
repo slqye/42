e759a116d993349370e222b541ac11288c011fac
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-05 15:57:50 +0100

    feat: good abstract class Agent

--- sources/learn2slither/agent.py
+++ sources/learn2slither/agent.py
@@ -1,11 +1,11 @@
+import abc
+import json
 import random
 
-class Agent:
-	ACTION_COUNT = 4
-
+class Agent(abc.ABC):
 	def __init__(self, config: dict, interpreter: object) -> None:
 		self.config: dict = config
-		self._interpreter = interpreter
+		self.interpreter = interpreter
 		self._q_table: dict = {}
 
 	def learn(self, environment: object) -> bool:
@@ -23,11 +23,31 @@ class Agent:
 			yield False
 		yield True
 
+	def play(self, environment: object) -> bool:
+		state: str = None
+		greed: float = 0
+		action: int = 0
+		reward: int = 0
+
+		while environment.state is True:
+			state = self._get_state(environment)
+			action = self._perform_action(environment, state, greed)
+			yield False
+		yield True
+
+	def load(self, path: str) -> None:
+		with open(path, "r") as file:
+			self._q_table = json.load(file)
+
+	def save(self, path: str) -> None:
+		with open(path, "w") as file:
+			file.write(json.dumps(self._q_table, indent=4))
+
 	def _get_state(self, environment: object) -> str:
-		state: str = self._interpreter.compute_state(environment)
+		state: str = self.interpreter.compute_state(environment)
 
 		if state not in self._q_table:
-			self._q_table[state] = [0.0] * self.ACTION_COUNT
+			self._q_table[state] = [0.0] * environment.action_space
 		return state
 
 	def _perform_action(self, environment: object, state: str, greed: float) -> int:
@@ -37,22 +57,16 @@ class Agent:
 		action: int = 0
 
 		if random.random() < greed:
-			q_value = random.randint(0, self.ACTION_COUNT - 1)
+			action = random.randint(0, environment.action_space - 1)
 		else:
 			best_actions = [index for index, action in enumerate(q_values) if action == q_value]
 			action = random.choice(best_actions)
 		environment.action(action)
 		return action
 
+	@abc.abstractmethod
 	def _get_reward(self, environment: object) -> None:
-		rewards: dict = {
-			environment.EVENT_NOTHING: self.config["rewards"]["nothing"],
-			environment.EVENT_DIE: self.config["rewards"]["die"],
-			environment.EVENT_GROW: self.config["rewards"]["grow"],
-			environment.EVENT_SHRINK: self.config["rewards"]["shrink"],
-		}
-
-		return rewards[environment.event]
+		pass
 
 	def _update_q_table(self, environment: object, state: str, action: int, reward: float) -> None:
 		current_q_value: float = self._q_table[state][action]
@@ -66,3 +80,17 @@ class Agent:
 			target += self.config["gamma"] * max(self._q_table[new_state])
 		updated_q_value += self.config["learning_rate"] * (target - current_q_value)
 		self._q_table[state][action] = round(updated_q_value, self.config["precision"])
+
+class SnakeAgent(Agent):
+	def __init__(self, config: dict, interpreter: object) -> None:
+		super().__init__(config, interpreter)
+
+	def _get_reward(self, environment: object) -> None:
+		rewards: dict = {
+			environment.EVENT_NOTHING: self.config["rewards"]["nothing"],
+			environment.EVENT_DIE: self.config["rewards"]["die"],
+			environment.EVENT_GROW: self.config["rewards"]["grow"],
+			environment.EVENT_SHRINK: self.config["rewards"]["shrink"],
+		}
+
+		return rewards[environment.event]
